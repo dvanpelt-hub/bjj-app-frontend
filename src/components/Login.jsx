af9b0b0029f15c372d626b5ea9856d54f8e25c5e
dvanpelt-hub <vanpeltdh1@gmail.com>
2025-03-16 19:05:05 +0000

    set login state info

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,66 +1,73 @@
-import React from 'react';
-import {Link} from "react-router"
-import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Button, Checkbox, Form, Input, Flex } from 'antd';
+import React, {useEffect} from "react";
+import { Link } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { setCurrentUser } from "../redux/slices/currentUserSlice";
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
+import { Button, Checkbox, Form, Input, Flex } from "antd";
 
 const Login = () => {
-  
+  const dispatch = useDispatch();
+  const currentUser = useSelector((state) => state.currentUser.currentUser);
   const onFinish = (values) => {
-    console.log('Received values of form: ', values);
+    dispatch(setCurrentUser({ username: values.username }));
   };
 
   return (
     <>
-   <h2 className="m-2 text-xl">Login/Sign Up</h2> 
-    <Form
-      name="login"
-      initialValues={{
-        remember: true,
-      }}
-      style={{
-        maxWidth: 320,
-        margin: 'auto',
-      }}
-      onFinish={onFinish}
-    >
-      <Form.Item
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Username!',
-          },
-        ]}
+      <h2 className="m-2 text-xl">Login/Sign Up</h2>
+      <Form
+        name="login"
+        initialValues={{
+          remember: true,
+        }}
+        style={{
+          maxWidth: 320,
+          margin: "auto",
+        }}
+        onFinish={onFinish}
       >
-        <Input prefix={<UserOutlined />} placeholder="Username" />
-      </Form.Item>
-      <Form.Item
-        name="password"
-        rules={[
-          {
-            required: true,
-            message: 'Please input your Password!',
-          },
-        ]}
-      >
-        <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-      </Form.Item>
-      <Form.Item>
-        <Flex justify="space-between" align="center">
-          <Form.Item name="remember" valuePropName="checked" noStyle>
-            <Checkbox className="!text-[#ffffff]">Remember me</Checkbox>
-          </Form.Item>
-          <a href="">Forgot password</a>
-        </Flex>
-      </Form.Item>
+        <Form.Item
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Username!",
+            },
+          ]}
+        >
+          <Input prefix={<UserOutlined />} placeholder="Username" />
+        </Form.Item>
+        <Form.Item
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your Password!",
+            },
+          ]}
+        >
+          <Input
+            prefix={<LockOutlined />}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Flex justify="space-between" align="center">
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox className="!text-[#ffffff]">Remember me</Checkbox>
+            </Form.Item>
+            <a href="">Forgot password</a>
+          </Flex>
+        </Form.Item>
 
-      <Form.Item>
-        <Button block type="primary" htmlType="submit">
-          Log in
-        </Button>
-        or <Link to="/register">Register now!</Link>
-      </Form.Item>
-    </Form>
+        <Form.Item>
+          <Button block type="primary" htmlType="submit">
+            Log in
+          </Button>
+          or <Link to="/register">Register now!</Link>
+        </Form.Item>
+      </Form>
     </>
   );
 };
