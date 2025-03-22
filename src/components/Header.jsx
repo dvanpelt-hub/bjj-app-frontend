c47b3c1ec770f00a3bb9142ade6018b495e7e50e
dvanpelt-hub <vanpeltdh1@gmail.com>
2025-03-22 08:48:14 +0000

    sign out

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,46 @@
 import React from "react";
 import { Link } from "react-router";
-import { UserOutlined } from "@ant-design/icons";
+import { Dropdown, Avatar } from "antd";
+import { UserOutlined, LogoutOutlined } from "@ant-design/icons";
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
+
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    // dispatch(logout());
+    navigate("/login");
+  };
+
+  const menuItems = [
+    {
+      key: "logout",
+      icon: <LogoutOutlined />,
+      label: <span onClick={handleLogout}>Sign Out</span>,
+    },
+  ];
+
   return (
     <div className="flex items-center justify-between p-4">
       <Link to="/">
-        <h1 className="text-2xl text-white font-bold font-home-video">BJJ Review</h1>
+        <h1 className="text-2xl text-white font-bold font-home-video">
+          Re:Roll
+        </h1>
       </Link>
-      <UserOutlined />
+      <Dropdown
+        menu={{ items: menuItems }}
+        trigger={["click"]}
+        placement="bottomRight"
+      >
+        <Avatar
+          size="medium"
+          icon={<UserOutlined />}
+          style={{
+            cursor: "pointer",
+          }}
+        />
+      </Dropdown>
     </div>
   );
 };
--- src/utils/authChecker.js
+++ src/utils/authChecker.js
@@ -5,9 +5,11 @@ import { setToken } from "../redux/slices/authSlice";
 const AuthChecker = () => {
   const dispatch = useDispatch();
 
+  // checks if access token (jwt) exists on initial app render
   useEffect(() => {
     const access_token = localStorage.getItem("access_token");
     if (access_token) {
+      // if exists, set token in state
       dispatch(setToken(access_token));
     }
   }, [dispatch]);
