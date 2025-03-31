1dc5c9357f9b58f35472068eef733518c8f1e817
dvanpelt-hub <vanpeltdh1@gmail.com>
2025-03-31 09:40:51 +0100

    added scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter } from "react-router-dom";
 import BJJRouter from "./router/BJJRouter";
-import Header from "./components/Header"
+import Header from "./components/Header";
+import ScrollToTop from "./components/ScrollToTop"
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop />
     <Header />
       <BJJRouter />
     </BrowserRouter>
